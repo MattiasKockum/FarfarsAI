7303fa75ae4c445f49b2edf83b8a6cfc4ab94e38
Mattias <mattias@kockum.net>
2023-08-07 01:08:26 +0200

    Trying to comment things out

--- server/main.js
+++ server/main.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
+//const express = require('express');
+//const app = express();
+//const port = process.env.PORT || 5000;
 
 // Serve static files from the 'client' directory
-app.use(express.static('client'));
+//app.use(express.static('client'));
 
-const apiKey = process.env.GPT_KEY
+//const apiKey = process.env.GPT_KEY
 
-app.use(express.json());
+//app.use(express.json());
 
 app.post('/api/slice', (req, res) => {
   //const slicedKey = apiKey.slice(0, 3);
