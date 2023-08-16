a43111605d366730cfa56a4cbcfe92bfc2c30ff4
Mattias <mattias@kockum.net>
2023-08-16 22:22:51 +0200

    Working on the backend

--- server/main.js
+++ server/main.js
@@ -1,3 +1,14 @@
+exports.handler = async (event) => {
+    const { num1, num2 } = JSON.parse(event.body);
+
+    const result = num1 + num2;
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ result }),
+    };
+};
+
 //const express = require('express');
 //const app = express();
 //const port = process.env.PORT || 5000;
@@ -9,12 +20,12 @@
 
 //app.use(express.json());
 
-app.post('/api/slice', (req, res) => {
+//app.post('/api/slice', (req, res) => {
   //const slicedKey = apiKey.slice(0, 3);
   //res.json({ firstThreeCharacters: slicedKey });
-  res.json({firstThreeCharacters: "ABD"})
-});
+  //res.json({firstThreeCharacters: "ABD"})
+//});
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+//app.listen(port, () => {
+  //console.log(`Server is running on port ${port}`);
+//});
